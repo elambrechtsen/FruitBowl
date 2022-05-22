c6ffa5976cddfc186cbaace08c68b18359b5c0c5
elambrechtsen <eloise.lambrechtsen@marsden.school.nz>
2022-05-23 09:01:24 +1200

    This is my third sprint where you can change the quantity of fruit in the bowl

--- main_fruit.py
+++ main_fruit.py
@@ -12,6 +12,11 @@ def review_fruit(l):
         print(output)
     return None
 
+def print_with_indexes(l):
+    for i in range (0, len(l)):
+        output = " {:3} : {:10} {:10}".format(i, l[i][0], l[i][1])
+        print(output)
+
 def add_new_entry(l):
     #print("Start adding new fruit")
     fruit_name = get_string("Please enter the new fruit: ")
@@ -20,6 +25,16 @@ def add_new_entry(l):
     l.append(new_list)
     #return None
 
+def update_fruit(l):
+    print_with_indexes (l)
+    user_choice = get_integer("PLease enter the index number to update the name: ")
+    #print(L[my_index])
+    new_fruit = get_string("Please enter the new quantity of fruit: ")
+    old_fruit = l[user_choice][1]
+    l[user_choice][1] = new_fruit
+    #print(l [my_index][1])
+    output_message = "{} many has now been changed to {}".format(old_fruit, new_fruit)
+    print(output_message)
 
 def main():
     fruit_list = [
@@ -30,6 +45,7 @@ def main():
     menu_list = [
         ["R", "Review fruit"],
         ["A", "Add new fruit"],
+        ["C", "Change quantity of fruit"],
         ["Q", "Quit"]
         ]
     #add_new_entry(fruit_list)
@@ -45,6 +61,8 @@ def main():
             print(fruit_list)
         elif user_choice == "A":
             run_program ==(add_new_entry(fruit_list))
+        elif user_choice == "C":
+            run_program ==(update_fruit(fruit_list))
         elif user_choice == "Q":
             run_program = False
         else:
